85e61c8aa09f3ef86aeaff2dbd7a971293bab7cd
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-04-24 23:05:42 +0300

    Add one test.

--- tests/test_future.py
+++ tests/test_future.py
@@ -1,1 +1,14 @@
+import pytest
+
 from wraps.future import Future
+
+
+@pytest.mark.anyio
+async def test_future_from_value() -> None:
+    value = 42
+
+    future = Future.from_value(value)
+
+    result = await future
+
+    assert result is value
